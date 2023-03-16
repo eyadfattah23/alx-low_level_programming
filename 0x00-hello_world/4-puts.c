7654e024c3462fd7297ef62a2831e34127b2027c
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-16 13:53:18 -0700

    put function to print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing string using puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(string);
+	return (0);
+}
+
