2afe2622cf7a09da494e9d819239a89f96d39583
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-17 14:29:44 -0700

    reverse letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printf to print string
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
