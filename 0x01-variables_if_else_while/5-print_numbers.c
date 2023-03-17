545edd1d6676e5c3c99966f227718cdf14fcb8b3
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-17 13:49:56 -0700

    print nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printf to print string
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
