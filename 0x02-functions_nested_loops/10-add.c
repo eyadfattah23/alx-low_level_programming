869ba73484d4576a7a1982e2ba2c11642d47ded3
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-21 17:55:01 -0700

    add func.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - sum of 2 integers
+ * @a: int ot be added
+ * @b: int to be added
+ * Return: sum of a and b (success)
+ */
+int add(int a, int b)
+{
+        return (a + b);
+}
