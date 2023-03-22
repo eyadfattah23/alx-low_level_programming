3540a13429d2fea166b1fa2c6ec9c30ccc2a3dbc
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-21 18:14:45 -0700

    all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n++;
 		}
-		printf("%d", n);
+		printf("%d\n", n);
 	}
 	else if (n > 98)
 	{
@@ -26,6 +26,6 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n--;
 		}
-		printf("%d", n);
+		printf("%d\n", n);
 	}
 }
