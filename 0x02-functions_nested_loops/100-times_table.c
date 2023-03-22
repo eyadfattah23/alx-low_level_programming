c33cdb66fac7d9d4a856681c389f027422883b5f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-21 19:06:52 -0700

    prints the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,16 +25,14 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100)
+				} else if (res >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((res / 100) + '0');
 					_putchar(((res / 10) % 10) + '0');
 					_putchar((res % 10) + '0');
-				}
-				else if (res < 10 && i != 0)
+				} else if (res < 10 && i != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
