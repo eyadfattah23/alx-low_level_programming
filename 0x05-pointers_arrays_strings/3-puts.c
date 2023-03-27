57cc601e23ec2fb06e7cd247ba14ec0c5d40937f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-27 15:24:33 -0700

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,6 +2,7 @@
 
 /**
  * _puts - prints a string
+ * @str: str to be printed
  */
 
 void _puts(char *str)
@@ -13,4 +14,5 @@ void _puts(char *str)
 		_putchar(*(str + i));
 		i++;
 	}
+	_putchar('\n');
 }
