2cef7a4c1eb89813f02d639119fafd467a88abf5
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-27 15:57:34 -0700

    prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,17 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
 		i++;
+		s++;
 	}
-	while (i >= 0)
+	s--;
+	while (i > 0)
 	{
-		_putchar(*(s + i));
+		_putchar(*s);
 		i--;
+		s--;
 	}
 	_putchar('\n');
 }
