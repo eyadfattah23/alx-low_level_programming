4d0f566265ec24f0a1f17e5cb100ad741ee8306c
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-28 21:58:06 -0700

    concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to be measured
+ * Return: len of string;
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: appended to str
+ * @src: appended str
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int ldst, lsrc, i, j;
+
+	ldst = _strlen(dest);
+	lsrc = _strlen(src);
+	i = ldst;
+	j = 0;
+
+	while (i < ldst + lsrc)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
