1b20f32c4441069e819558d818e49374d39e13a4
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-03-29 21:50:25 -0700

    encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s: string to be measured
- * Return: len of string;
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
  * leet - encodes a string into 1337.
  * @s: str to be encoded
  * Return: encoded string
@@ -29,13 +12,13 @@ char *leet(char *s)
 	char *es = "4433007711";
 	int i, j;
 
-	for (i = 0; i < _strlen(s); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == e[j])
 				s[i] = es[j];
-		}	
+		}
 	}
 	return (s);
 }
