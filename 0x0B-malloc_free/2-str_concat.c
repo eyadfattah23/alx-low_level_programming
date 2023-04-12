3df97dd31945d92971ecb9cae9e38a519f0c95c8
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-12 03:43:29 +0200

    concatenates

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to be measured
+ * Return: len of string;
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: the string to concatenate
+ * @s2: the string to conc
+ * Return: the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int lens1 = _strlen(s1);
+	int lens2 = _strlen(s2);
+	int i = 0, j = 0;
+	char *result = malloc(lens1 + lens2 + 1);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+
+	while (i < lens1)
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (j < lens2)
+	{
+		result[i] = s2[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (result);
+}
