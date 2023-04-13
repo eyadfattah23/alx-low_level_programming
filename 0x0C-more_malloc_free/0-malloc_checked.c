3868fce6612351af6922bcd810ddbff4ca9cdf33
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-13 23:55:38 +0200

    allocates memory with exit status 98

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,8 @@
 /**
  * malloc_checked - allocates memory
  * @b: amount of memory to allocate
- * Return: pointer to the allocated memory
+ * Return: pointer to the allocated memory or .
+ * cause normal process termination with a status value of 98
  */
 void *malloc_checked(unsigned int b)
 {
