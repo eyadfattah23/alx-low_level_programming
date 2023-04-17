c12f991a65e6d7051a46a11678f88694d7995212
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-17 23:07:39 +0200

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,6 +32,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	
 	if (!new_ptr)
 	{
 		return (NULL);
