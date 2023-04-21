6426d0d4c97732b04e5a8032e217222f9c72a40e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-21 02:55:42 +0200

    prints numbers, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 				printf("%d\n", va_arg(args, int));
 			}
 			else
-				printf("%d ", va_arg(args, int));
+				printf("%d", va_arg(args, int));
 		}
 		else
 		{
