78da2e465867e420a6b61675b9c4ee70f978094c
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-28 08:50:18 +0300

    betty update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include "lists.h"
-
-void before_main (void)
+/**
+ * before_main - prints
+ * "You're beat! and yet, you must allow,\nI
+ * bore my house upon my back!\n"
+ * before the main function is executed.
+ */
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
