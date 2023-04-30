11871140bb712d28147890f9a96e9774162aac15
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-30 07:51:06 +0300

    revert

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (i = 0; (i < index - 1) && c_node; i++)
 		c_node = c_node->next;
 
-	if (!(c_node->next) || !c_node)
+	if (!(c_node->next) || (i < index - 1) || !c_node)
 	{
 		return (-1);
 	}
