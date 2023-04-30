6309bd4a1e868c695b6c5a750844f8568a3b7945
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-04-30 07:46:05 +0300

    handling empty list and out of bounds idx

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *c_node, *d_node;
 	unsigned int i;
 
-	if (!head)
+	if (!head || !*head)
 		return (-1);
 
 	c_node = *head;
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (i = 0; (i < index - 1) && c_node; i++)
 		c_node = c_node->next;
 
-	if (!(c_node->next) || (i < index - 1))
+	if (!(c_node->next) || (i < index - 1) || !c_node)
 	{
 		return (-1);
 	}
