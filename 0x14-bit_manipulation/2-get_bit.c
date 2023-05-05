a2b1a92540987814926f7fc89bfa2ac109da214e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-05-05 03:20:16 +0300

    ....

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,37 +1,6 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers.
- * @a: num to be swapped
- * @b: num to be swapped
- */
-
-void swap_int(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * reverse_array - reverses the content of an array of integers
- * @a: array to be reversed
- * @n: size of array
- */
-
-void reverse_array(int *a, int n)
-{
-	int i, j = n - 1;
-
-	for (i = 0; i < j; i++, j--)
-	{
-		swap_int(&a[i], &a[j]);
-	}
-
-}
-/**
  * get_bit - returns the value of a bit at a given index.
  * @n: number to search in
  * @index: the index
@@ -39,21 +8,10 @@ void reverse_array(int *a, int n)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bin_arr[32];
-	unsigned int j, i = 0;
-
-	while (n > 0)
+	if (index >= 32)
 	{
-		bin_arr[i] = n % 2;
-		n /= 2;
-		i++;
+		return (-1);
 	}
-	for (j = i - 1; j >= index; j--)
-	{
-		if (j == index)
-		{
-			return (bin_arr[j]);
-		}
-	}
+	return ((n >> index) & 1);
 	return (-1);
 }
