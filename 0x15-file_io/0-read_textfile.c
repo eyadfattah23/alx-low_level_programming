3aa285cf4683e3c12538fbcc870141c1f662bc89
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-05-10 05:12:05 +0300

    revert

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -33,7 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	wb = write(1, buffer, letters);
+	wb = write(1, buffer, rb);
 	if (wb == -1 || (wb != rb))
 	{
 		free(buffer);
