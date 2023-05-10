fd125b32c87138eba912ebb15d81477df4e7781b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-05-10 06:22:13 +0300

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 		l = _strlen(text_content);
-	
+
 	wb = write(file, text_content, l);
 	if (wb == -1)
 		return (-1);
