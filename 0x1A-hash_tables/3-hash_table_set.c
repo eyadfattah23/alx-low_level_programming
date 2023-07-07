2ebb111bb6af23af3428b424a47a2c6acf32747d
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-07-07 11:05:14 +0300

    add null conditions handling

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -63,7 +63,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free_hash_node(new);
 		return (0);
 	}
-	if (!new)
+	if (!new || !key || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
