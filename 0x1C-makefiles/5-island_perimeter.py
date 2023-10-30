022992d2efa297ac1e1ec66e07bb739e5eeb8a34
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-10-30 14:49:44 +0200

    pyc style

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,7 +7,7 @@ def island_perimeter(grid):
     """ returns the perimeter of the island described in grid
 
     Args:
-        grid (list of list of integers): 
+        grid (list of list of integers):
     0 represents a water zone
     1 represents a land zone
     One cell is a square with side length 1
@@ -24,13 +24,17 @@ def island_perimeter(grid):
             zone_per = 0
             if grid[row_index][zone_index] == 1:
                 zone_per = 4
-                if (row_index + 1) < len(grid) and grid[row_index + 1][zone_index] == 1:
+                if (row_index + 1) < len(grid) \
+                        and grid[row_index + 1][zone_index] == 1:
                     zone_per -= 1
-                if (row_index - 1) >= 0 and grid[row_index - 1][zone_index] == 1:
+                if (row_index - 1) >= 0 \
+                        and grid[row_index - 1][zone_index] == 1:
                     zone_per -= 1
-                if (zone_index + 1) < len(grid[row_index]) and grid[row_index][zone_index + 1] == 1:
+                if (zone_index + 1) < len(grid[row_index]) \
+                        and grid[row_index][zone_index + 1] == 1:
                     zone_per -= 1
-                if (zone_index - 1) >= 0 and grid[row_index][zone_index - 1] == 1:
+                if (zone_index - 1) >= 0 \
+                        and grid[row_index][zone_index - 1] == 1:
                     zone_per -= 1
             per += zone_per
     return per
