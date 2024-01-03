c9dae9182e166e2a2a0996d41b9805f5acbfff00
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-03 20:47:40 +0200

    defer to 16

--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEARCH_ALGS_H
-#define SEARCH_ALGS_H
-
-#include <stdlib.h>
-#include <stdio.h>
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-#endif
