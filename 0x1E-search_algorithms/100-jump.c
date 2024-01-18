8efc892161c595e676eddd54b56f23590daf8d0f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-01-18 17:12:12 +0200

    handle value not in array and smaller than size - 1

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,6 +30,7 @@ int jump_search(int *array, size_t size, int value)
 					return (j);
 				}
 			}
+			return (-1);
 		}
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", size - 1, next);
